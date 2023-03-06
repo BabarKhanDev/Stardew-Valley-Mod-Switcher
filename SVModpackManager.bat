1d721b9510c2c31f0127474933d41437a6a8269b
Babar Khan <87698523+BabarKhanDev@users.noreply.github.com>
2023-03-06 19:34:08 +0000

    Add code to launch stardew

--- SVModpackManager.bat
+++ SVModpackManager.bat
@@ -7,4 +7,6 @@ set mods_dir="R:/Games/Steam/steamapps/common/Stardew Valley/Mods/"
 
 python "nogui.py" %modpacks_dir% %mods_dir%
 
-rem Pause at the end of the script (optional)
+set steam="C:/Games/Steam/steam.exe"
+
+%steam% "steam://rungameid/413150"
